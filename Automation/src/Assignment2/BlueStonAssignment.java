4c0a42a4509bd5d18fdced77b61b857d252a1663
Hp <Hp@DESKTOP-U0EDMT2>
2024-05-23 12:04:52 +0530

    bluestone

--- Automation/src/Assignment2/BlueStonAssignment.java
+++ Automation/src/Assignment2/BlueStonAssignment.java
@@ -1,15 +1,21 @@
 package Assignment2;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.google.common.io.Files;
+
 public class BlueStonAssignment {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		 WebDriver driver=new ChromeDriver();
 		 driver.manage().window().maximize();
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -32,6 +38,11 @@ public class BlueStonAssignment {
 			  }
 		
 		  }
+		  TakesScreenshot ts=(TakesScreenshot)driver;
+		  File src = ts.getScreenshotAs(OutputType.FILE);
+		  File dest=new File("./screenshot/coin.png");
+		  Files.copy(src, dest);
+		  
 	}
 }
 		  
